2ab7753de10789cfd763315f1acf35a3e2c7e394
stevenwu99 <stevenmaster@hotmail.com>
2023-07-19 10:12:36 +0100

    Task6 delete a comment code

--- src/components/ShowCommentsByArticle.jsx
+++ src/components/ShowCommentsByArticle.jsx
@@ -3,7 +3,6 @@ import { getData } from "../utilities/API";
 import CommentCard from "./CommentCard";
 
 const ShowCommentsByArticle = ({article_id}) =>{
-    //const [article_id] = useOutletContext();
 
     const [comments,setComments] = useState([]);
     const [isLoading,setIsLoading] = useState(true)
