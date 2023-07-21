b5d5be0260aa2fe3f7a14db4b895f7e10accc776
stevenwu99 <stevenmaster@hotmail.com>
2023-07-21 14:57:37 +0100

    Task 12 Error handling

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return (
+       <div>
+           <p>The resource is unavailable.</p>
+           <p>Please contact NC news App admin.</p>
+           <p>Email: stevenmaster@hotmail.com</p>
+       </div>
+    )
+}
+
+export default ErrorPage;
